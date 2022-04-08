720e575f2ebdfa2e71d301dadaa860ecab1d8b57
Manuel Chavarria <85698560+mchavarriap628@users.noreply.github.com>
2022-04-08 14:26:31 -0600

    Primera version
    - Login
    - Icono de la APP
    - Barra de estado color negra
    - Color de focus en los entry
    - Detalles

---
+++ TouristCR/TouristCR/TouristCR/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TouristCR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnLogin.Clicked += BtnLogin_Clicked;
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            if (EntEmail.Text == "manuel@ya.com" & EntPass.Text == "contraseña")
+            {
+                this.Navigation.PushModalAsync(new Menu());
+
+                EntEmail.Text = "";
+                EntPass.Text = "";
+            }
+            else
+            {
+                mensajeEmergenteAsync("Authentication Error", "Please verify the password or email are valid.");
+            }
+        }
+
+        public async Task mensajeEmergenteAsync(String titulo, String mensaje)
+        {
+            await DisplayAlert(titulo, mensaje, "OK");
+        }
+
+
+
+    }
+}
