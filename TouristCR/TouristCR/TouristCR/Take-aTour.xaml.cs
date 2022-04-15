8996aded6fbf80ea38f453af7db965d6bbcec61e
Benjamin Samuel Powoon Salas <bpowoons890@ulacit.ed.cr>
2022-04-14 19:06:31 -0600

    tab de take a tour, con fecha, hora y precios, hay falta a comodarlo de modo que se pueda llamar desde la aplicacion del gutHub. Ya que lo hice con codigo aparte.

---
+++ TouristCR/TouristCR/TouristCR/Take-aTour.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Base22
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Take_aTour : TabbedPage
+    {
+        public Take_aTour()
+        {
+            InitializeComponent();
+            btSiguienteFecha.Clicked += BtSiguienteFecha_Clicked;
+            btSiguenteTiempos.Clicked += BtSiguenteTiempos_Clicked;
+            btnSiguienteTarifas.Clicked += BtnSiguienteTarifas_Clicked;
+            btnSalir.Clicked += BtnSalir_Clicked;
+        }
+
+        private void BtSiguenteTiempos_Clicked(object sender, EventArgs e)
+        {
+            var pages = Children.GetEnumerator();
+            pages.MoveNext();
+            pages.MoveNext();
+            pages.MoveNext();
+            CurrentPage = pages.Current;
+        }
+
+        private void BtnSalir_Clicked(object sender, EventArgs e)
+        {
+            
+           // ((NavigationPage)this.Parent).PushAsync(new MainPage());
+        }
+
+        private void BtnSiguienteTarifas_Clicked(object sender, EventArgs e)
+        {
+            var pages = Children.GetEnumerator();
+            pages.MoveNext();
+           // pages.MoveNext();
+            CurrentPage = pages.Current;
+        }
+
+      
+
+        private void BtSiguienteFecha_Clicked(object sender, EventArgs e)
+        {
+            var pages = Children.GetEnumerator();
+            pages.MoveNext(); //First page
+            pages.MoveNext(); //second page
+            //pages.MoveNext();
+            CurrentPage = pages.Current;
+        }
+    }
+}
